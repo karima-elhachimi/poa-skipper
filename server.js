dda65355cfbea05452166fbaaff08de6bfbd5aae
Karima El Hachimi <karima.elhachimi@hotmail.com>
2019-04-24 15:14:38 +0200

    fixed port issue

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
 const app = require('./app')
 
-app.listen(process.env.PORT || 8080)
+app.listen(process.env.PORT || 8080, () => {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
