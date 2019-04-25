ce1d01c267ed9fbeb5b1e6a0e077b172bf5ce180
Karima El Hachimi <karima.elhachimi@hotmail.com>
2019-04-25 11:30:56 +0200

    removed duplicate code

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const app = express()
 app.use(cors());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", '*');
-  res.header("Access-Control-Allow-Credentials", true);
+  //res.header("Access-Control-Allow-Credentials", true);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
   next();
@@ -52,13 +52,6 @@ let lcArray = [
 ];
 const lockCodeMap = new Map(lcArray);
 
-
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 
 app.get('/chat/:text', (req, res) =>{
