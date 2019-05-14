02476f95eecb5c9e0c038f205acb9e113f00a2ee
Karima El Hachimi <karima.elhachimi@hotmail.com>
2019-05-14 15:21:08 +0200

    parsed json response for foreachcall

--- fulfillmenthelpers.js
+++ fulfillmenthelpers.js
@@ -87,7 +87,7 @@ module.exports = class FulfillmentHelpers {
         return this.requestApicsData(url)
         .then(res => {
             console.log(`#requestApicsData response: ${res}`);
-            return this.formatAvailableQuay(res);
+            return this.formatAvailableQuay(JSON.parse(res));
         })
     }
 
