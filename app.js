98cd798ff6de4afa07057e57e6e6d06929ce2518
Karima El Hachimi <karima.elhachimi@hotmail.com>
2019-05-14 16:51:08 +0200

    changed response for availableQuays

--- app.js
+++ app.js
@@ -133,7 +133,7 @@ app.post('/fulfillment', express.json(), (request, response) => {
     return fulfill.requestAvailableQuays(location)
     .then(res => {
       console.log(`#respondWithAvailableQuay: ${res}`);
-      agent.add(`available quay: ${res}`);
+      agent.add(`Volgende kaainummers zijn beschikbaar: \n ${res}`);
     })
   }
 
